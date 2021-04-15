49b791e6e72122e881863d1000e5bf66e01adeb1
fredlc <fredlefevre@outlook.com>
2021-04-15 11:01:07 -0400

    Implemented delete action and button for artists, refactored models into separate file and configured additional constraints for all models

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ import json
 import dateutil.parser
 from datetime import datetime
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort, jsonify
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from flask_migrate import Migrate
 from forms import *
 from config import basedir
+from models import db, Venue, Artist, Show
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -23,53 +23,9 @@ from config import basedir
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 
 migrate = Migrate(app, db)
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
-    shows = db.relationship('Show', backref='venue', lazy=True)
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
-    shows = db.relationship('Show', backref='artist', lazy=True)
-
-class Show(db.Model):
-  __tablename__ = 'Show'
-
-  id = db.Column(db.Integer, primary_key=True)
-  start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -183,8 +139,6 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>/delete', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
   error = False
   venue = Venue.query.get(venue_id)
   try:
@@ -194,16 +148,14 @@ def delete_venue(venue_id):
     db.session.rollback()
     error = True
     flash('An error occurred. Venue could not be deleted.')
-    return 'Failed'
-    # return redirect(url_for('index'))
+    return jsonify({'success': False})
   finally:
     db.session.close()
   if error:
     abort(500)
   else:
     flash('Venue was successfully deleted!')
-    return 'Success'
-    # return redirect(url_for('index'))
+    return jsonify({'success': True})
   
 
 #  Artists
@@ -241,6 +193,27 @@ def show_artist(artist_id):
   append_venue_data(past_shows, past_shows_data)
   
   return render_template('pages/show_artist.html', artist=artist, upcoming_shows_data=upcoming_shows_data, past_shows_data=past_shows_data)
+
+
+@app.route('/artists/<artist_id>/delete', methods=['DELETE'])
+def delete_artist(artist_id):
+  error = False
+  artist = Artist.query.get(artist_id)
+  try:
+    db.session.delete(artist)
+    db.session.commit()
+  except:
+    db.session.rollback()
+    error = True
+    flash('An error occurred. Artist could not be deleted.')
+    return jsonify({'success': False})
+  finally:
+    db.session.close()
+  if error:
+    abort(500)
+  else:
+    flash('Artist was successfully deleted!')
+    return jsonify({'success': True})
 
 #  Update
 #  ----------------------------------------------------------------
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -6,9 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
+# Database configuration
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost:5432/fyyur'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
